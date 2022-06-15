62fb5d97153da8bca4160a16272cffae8c866b0f
Sami Bajwa <55937870+samixtures@users.noreply.github.com>
2022-06-14 17:31:26 -0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    consumer_key: 'd46b6fenFxLwGWJOzR53AgVQn',
-    consumer_secret: 'tlYWBCCtnsiwlL7fNwfHeT72WoYLEpDrG7BKXZqYVXhwrHuK8E',
-    access_token_key: '1536378177466925056-t3zXwt4rOzsgLfRC7yKEdCrVYKyP9z',
-    access_token_secret:  'uuQuZd73qRBFMFXr2QdfSoGtLL8xe2lbkgMhCToXzfENo',
-    // bearer_token: 'AAAAAAAAAAAAAAAAAAAAADLRdgEAAAAA4%2BY7J%2BNoF9EveIPPzAMnlede%2Fso%3DdnAvCd4lmwCnE3tNjNaAnoRGukCb6Ef5jo6Y8yNMZYh6Uac01i',
-}
